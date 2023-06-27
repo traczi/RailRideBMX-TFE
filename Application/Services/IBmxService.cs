9e3e0b994bd59f696e0ed20d3d4e735b0c8774ab
traczi <matteo.alcantarini@hotmail.com>
2023-06-27 23:23:42 +0200

    frist commit

---
+++ Application/Services/IBmxService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Models.Bmx;
+
+namespace Application.Services;
+
+public interface IBmxService
+{
+    Task<IEnumerable<BmxResponseModel>> GetAllBmxAsync();
+    Task<BmxResponseModel> CreateBmxAsync(BmxResponseModel bmxResponseModel);
+    Task<BmxResponseModel> GetBmxByIdAsync(Guid guid);
+    Task<BmxResponseModel> DeleteBmx(Guid guid);
+    Task<BmxResponseModel> UpdateBmx(Guid guid,  BmxResponseModel bmxResponseModel);
+}
---
+++ DataAccess/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Persistence;
+
+public class ApplicationDbContext : DbContext
+{   
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Bmx> Bmxs { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Bmx>().HasKey(b => b.Id);
+    }
+}
